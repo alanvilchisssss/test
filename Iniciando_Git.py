374c5f96a34215b1d3eaddfc624f503ba70adbf5
Alan Vilchis <alanvilchis07@gmail.com>
2024-07-09 20:23:12 -0600

    mi primer commit

---
+++ Iniciando_Git.py
@@ -0,0 +1,8 @@
+import random
+print("Estoy usando git")
+print("segunda actualización")
+lista=[{1,2,3},{1,2,3},{4,5,6}]
+variable=random.randint(0,2)
+print("{}".format(lista))
+for i in lista:
+    print("{}".format(1,2))
